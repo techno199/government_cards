e2111591822e743e4de54df8f376bb4c095473a0
Alexander Tishkov <techno199@live.ru>
2019-10-10 13:20:14 +0300

    Updates

---
+++ precache-manifest.5e4334f4861f0633f7c7754f419b3f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd83423decd06bd39b2cc785f835a3d",
+    "url": "/government_cards/index.html"
+  },
+  {
+    "revision": "482e92bdbfd7b107b84b",
+    "url": "/government_cards/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "42cf44427a5d49f6a249",
+    "url": "/government_cards/static/js/2.f7ee86d3.chunk.js"
+  },
+  {
+    "revision": "482e92bdbfd7b107b84b",
+    "url": "/government_cards/static/js/main.3f097e3f.chunk.js"
+  },
+  {
+    "revision": "b81bf6da997189f1257e",
+    "url": "/government_cards/static/js/runtime-main.e699cfad.js"
+  }
+]);
